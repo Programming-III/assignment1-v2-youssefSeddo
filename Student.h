4bb5cf1cc8e073a54ea7528071dab1819ea52a00
Youssef Elseddawy <yelseddawy@gmail.com>
2025-11-11 10:03:06 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,21 +1,14 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-
-#include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
-
-
-
-
-
-
+#include<iostream>
+using namespace std;
+class Student:public Person {
+    public:
+        int yearLevel;
+        string major;
+        Student(int yearLevel, string major,string name,int id);
+        Student();
+        void display();
+        int getYearLevel();
+        string getMajor();
 
 
-
-
-
-
-
-#endif
+};
